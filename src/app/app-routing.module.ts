47669be171351e92bec5423fa375c407baab2572
WalterGimenez <wallegimenez@gmail.com>
2022-03-20 22:02:40 -0300

    se agregó header, navbar, error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorPageComponent } from './component/error-page/error-page.component';
+import { HeaderComponent } from './component/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HeaderComponent, pathMatch: 'full'},
+  {path: '**', component: ErrorPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from './component/header/header.component';
+import { ErrorPageComponent } from './component/error-page/error-page.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ErrorPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
